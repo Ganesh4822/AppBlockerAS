56cf88c5f363ecfb8db2ad785a95676639c1d7f0
ganesh4822 <ganesh.ingale4822@gmail.com>
2025-04-29 22:35:35 +0530

    Test changes to correct the author

--- AppBlockerv3/app/src/main/java/com/example/appblockerv3/ui/screens/CreateGroupScreen.kt
+++ AppBlockerv3/app/src/main/java/com/example/appblockerv3/ui/screens/CreateGroupScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
